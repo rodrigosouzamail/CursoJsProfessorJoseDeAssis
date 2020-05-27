aaa9155462b273b5e6ef8701bccd7d8ae36ce739
Rodrigo Souza <rodrigo.souzamail@gmail.com>
2020-05-27 12:11:33 -0300

    Primeiros passos com Java Script.
    Curso do professor josé de assis, aprendendo a trabalhar com JS.

---
+++ simplefunction/hellofunction.js
@@ -0,0 +1,11 @@
+/*
+JS - Aula 4: Função simples
+@author Rodrigo Souza
+*/
+
+function hello(){
+    var nome;
+    nome = prompt ("Qual é o seu nome ?");
+
+    window.alert("Hello " + nome)
+}
---
+++ vincularjs/documento.js
@@ -0,0 +1,10 @@
+/*
+
+*JS - Aula3: Vinculando um documento JS
+*@author Rodrigo Souza
+
+*/
+// document = é um objeto pre definido e ele representa toda a página web.
+
+//.write é um método do objeto document que escreve um conteúdo na página.
+document.write("<h2>" + "Rodrigo Souza" + "</h2>")
